3d3b0de5144993a8dc09975703e8f8c9414465de
martamasramon <marta.masramon@gmail.com>
2026-02-13 14:51:05 +0000

    changed sh script

--- downstream_tasks/csPCa_classification/run_cs_classification.sh
+++ downstream_tasks/csPCa_classification/run_cs_classification.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#$ -l tmem=64G,h_vmem=64G
+#$ -l gpu=true
+#$ -l h_rt=25:00:00
+
+#$ -S /bin/bash
+#$ -j y
+#$ -V
+
+#$ -wd /cluster/project7/ProsRegNet_CellCount/CriDiff/downstream_tasks/csPCa_classification/
 
 # Generate timestamp
 timestamp=$(date +%Y%m%d_%H%M%S)
@@ -17,6 +25,11 @@ done
 dir_name="outputs/$timestamp"
 mkdir -p "$dir_name"
 
-# Run the Python script, passing the timestamp as an argument
-# Redirect output to a log file inside the timestamped directory
+
+export PATH=/share/apps/python-3.9.5-shared/bin:$PATH
+export LD_LIBRARY_PATH=/share/apps/python-3.9.5-shared/lib:$LD_LIBRARY_PATH
+
+source /cluster/project7/ProsRegNet_CellCount/CriDiff/CriDiff_env/bin/activate
+export PATH="/cluster/project7/ProsRegNet_CellCount/CriDiff/CriDiff_env/bin:$PATH"
+
 PYTHONPATH=$(pwd) python ./train.py "$dir_name" "$timestamp" $DEBUG_FLAG > "$dir_name/training_inference_output_$timestamp.log" 2>&1
